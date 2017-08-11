ca6eecee6b7ff5c11b0195d3c8d5418ca28761ed
Herman Chan <hermchan89@gmail.com>
2017-08-10 17:15:16 -0700

    lodash

--- src/client/app/components/CharacterForm/CharacterForm.js
+++ src/client/app/components/CharacterForm/CharacterForm.js
@@ -1,10 +1,13 @@
+import _ from 'lodash'
 import React from 'react';
 import axios from 'axios';
 import './CharacterForm.scss';
 import CharacterInfo from '../CharacterInfo/CharacterInfo';
 import config from '../../../../config/config.js';
+import Button from '../commons/Button/Button';
 
 const wowkey = config.WOW_API_KEY;
+// let text = _.isUndefined(route.rightButtonText) ? 'Default value' : route.rightButtonText;
 
 class CharacterForm extends React.Component {
   constructor(props) {
@@ -45,6 +48,16 @@ class CharacterForm extends React.Component {
   }
 
   render() {
+    const x =
+      _.forEach([1, 2], function(value) {
+      console.log(value);
+      });
+      // => Logs `1` then `2`.
+
+      _.forEach({ 'a': 1, 'b': 2 }, function(value, key) {
+      console.log(key);
+    });
+
     return (
       <div className="bg-white">
         <div className="centralized">
@@ -53,6 +66,7 @@ class CharacterForm extends React.Component {
               <form onSubmit={this.handleSubmit}>
                 <input type="text" placeholder="Character Name" name="characterName" value={this.state.characterName} onChange={this.handleChange} />
                 <button type="submit" value="Submit">Search</button>
+                <Button default fluid type="submit">Search</Button>
                 <select id="realm" onChange={this.handleRealmChange} value={this.state.realm}>
                   <option value="proudmoore">Proudmoore</option>
                   <option value="emerald dream">Emerald Dream</option>
