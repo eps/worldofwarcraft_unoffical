cdb1b5a2e76df8a7b0cdb35ea068bab42ed45940
Kyle Huey <krh6189@gmail.com>
2017-08-10 17:17:47 -0700

    Revert "attemted to create a dynamic realm list"
    This reverts commit 828a4a9529cefc71126e9b4d7df293532bad864a.

--- src/client/app/components/CharacterForm/CharacterForm.js
+++ src/client/app/components/CharacterForm/CharacterForm.js
@@ -1,11 +1,8 @@
-const _ = require('lodash');
-const axios = require('axios');
-const React =require('react');
-
-const CharacterInfo = require('../CharacterInfo/CharacterInfo');
-const config = require('../../../../config/config.js');
-
-// import './CharacterForm.scss';
+import React from 'react';
+import axios from 'axios';
+import './CharacterForm.scss';
+import CharacterInfo from '../CharacterInfo/CharacterInfo';
+import config from '../../../../config/config.js';
 
 const wowkey = config.WOW_API_KEY;
 
@@ -13,13 +10,11 @@ class CharacterForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      characterName: '',
       realm: '',
-      realmList: [],
+      characterName: '',
       submitted: false
     };
 
-    this.getRealmList();
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleRealmChange = this.handleRealmChange.bind(this);
@@ -30,29 +25,16 @@ class CharacterForm extends React.Component {
     console.log(e.target.name, e.target.value);
   }
 
-  getRealmList() {
-    axios.get('https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=' + wowkey)
-      .then((res) => {
-        const realmNames = _.map(res, 'name');
-        console.log(realmNames);
-          this.setState = ({
-            realmList: realmNames
-          });
-        }).catch((err) => {
-          console.log(err);
-        });
-  }
-
   handleSubmit(event) {
     event.preventDefault();
     console.log('realm is', this.state.realm);
-    axios.get('https://us.api.battle.net/wow/character/' + this.state.realm + '/' + this.state.characterName + '?locale=en_US&apikey=' + wowkey)
-      .then((response) => {
-        this.setState({
-          profile: response.data,
-          submitted: true
-        });
-      }).catch((error) => {
+    axios.get('https://us.api.battle.net/wow/character/'+this.state.realm+ '/' + this.state.characterName + '?locale=en_US&apikey=' + wowkey ).then(response => {
+      this.setState({
+        profile: response.data,
+        submitted: true
+      });
+    })
+    .catch((error) => {
       console.log("error",error)
     })
   }
@@ -72,7 +54,9 @@ class CharacterForm extends React.Component {
                 <input type="text" placeholder="Character Name" name="characterName" value={this.state.characterName} onChange={this.handleChange} />
                 <button type="submit" value="Submit">Search</button>
                 <select id="realm" onChange={this.handleRealmChange} value={this.state.realm}>
-                  {this.state.realmList}
+                  <option value="proudmoore">Proudmoore</option>
+                  <option value="emerald dream">Emerald Dream</option>
+                  <option value="frostmourne">Frostmourne</option>
                 </select>
               </form>
             </div>
