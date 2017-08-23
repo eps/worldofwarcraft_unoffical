1324a2f9f7e243aa01cec4ed9dd2ac71a6b7dd0f
Kyle Huey <krh6189@gmail.com>
2017-08-23 16:40:48 -0700

    Stylized boss card, sorta

--- src/client/app/components/RaidProgression/BossCard/BossCard.js
+++ src/client/app/components/RaidProgression/BossCard/BossCard.js
@@ -5,16 +5,15 @@ class BossCard extends React.Component {
 
     render() {
         const { kills } = this.props;
-        console.log('props are', kills);
 
         return (
-            <bossCard className={styles.bossCard}>
+            <div className={styles.bossCard}>
                 <div className={styles.bossName}>Name: {kills.name}</div>
                 <div className={styles.bossName}>LFR Kills: {kills.lfrKills}</div>
                 <div className={styles.bossName}>Normal Kills: {kills.normalKills}</div>
                 <div className={styles.bossName}>Heroic Kills: {kills.heroicKills}</div>
                 <div className={styles.bossName}>Mythic Kills: {kills.mythicKills}</div>
-            </bossCard>
+            </div>
         );
     }
 }
