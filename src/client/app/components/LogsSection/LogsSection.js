cdb8980c8a4ac5ab2e20d5adefef8b26969b512c
Herman Chan <hermchan89@gmail.com>
2017-10-25 16:19:27 -0700

    render warcraft logs api

--- src/client/app/components/LogsSection/LogsSection.js
+++ src/client/app/components/LogsSection/LogsSection.js
@@ -10,11 +10,13 @@ class LogsSection extends React.Component {
     this.state = {
       showMythic: false,
       showHeroic: false,
-      showNormal: false
+      showNormal: false,
+      bossImageUrl: ''
     }
     this.mythic = this.mythic.bind(this);
     this.heroic = this.heroic.bind(this);
     this.normal = this.normal.bind(this);
+    this.toggleActive = this.toggleActive.bind(this);
   }
 
   mythic() {
@@ -39,9 +41,29 @@ class LogsSection extends React.Component {
     });
   }
 
+  componentDidMount() {
+    console.log('mount');
+    this.toggleActive();
+  }
+
+  toggleActive() {
+    console.log('running');
+    const result = _.map(this.props.logs, 'difficulty');
+    _.forEach(result, (difficult) => {
+      if (difficult == 5) {
+        this.mythic();
+      } else if (difficult == 4) {
+        this.heroic();
+      } else {
+        this.normal();
+      }
+    });
+  }
+
   render() {
-    const { logs } = this.props;
-    console.log(logs);
+    const { logs, progress } = this.props;
+    const bossKills = _.last(progress.raids).bosses;
+    const name = _.map(logs, 'name');
 
     return (
       <div className={styles.centralized}>
@@ -49,7 +71,7 @@ class LogsSection extends React.Component {
           <thead>
             <tr>
               <th className={styles.tableHead}>Warcraft Logs</th>
-              <th className={cx(styles.tableHead, styles.tableCenter)}>Best Hestorical Percentage</th>
+              <th className={cx(styles.tableHead, styles.tableCenter)}>Percentage</th>
               <th className={cx(styles.tableHead, styles.tableCenter)}>Equipped</th>
             </tr>
           </thead>
@@ -72,15 +94,13 @@ class LogsSection extends React.Component {
               </td>
             </tr>
           </tbody>
-
           <tbody>
-          {_.map(logs, (kills) =>
+          {_.map(logs, (log, index) => (
             <Logs
-              key={kills.id}
-              logs={kills}
-              specs={kills.specs}
+              log={log}
+              key={index}
             />
-          )}
+          ))}
           </tbody>
         </table>
       </div>
