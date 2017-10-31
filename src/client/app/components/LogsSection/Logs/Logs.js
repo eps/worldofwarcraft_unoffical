c8cd938d206cfa736284408f97e92285d6d2a098
Herman Chan <hermchan89@gmail.com>
2017-10-31 16:28:51 -0700

    fixed table cell formatting and remove unnecessary code

--- src/client/app/components/LogsSection/Logs/Logs.js
+++ src/client/app/components/LogsSection/Logs/Logs.js
@@ -11,32 +11,8 @@ const wowKey = config.WOW_API_KEY;
 class Logs extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      bossImageUrl: '',
-      icon: [],
-    }
   }
-  //
-  // componentWillMount() {
-  //   axios.get('https://us.api.battle.net/wow/boss/' + this.props.progress.id + '?locale=en_US&apikey=' + wowKey)
-  //     .then((res) => {
-  //       const creatureDisplayId = _.first(res.data.npcs).creatureDisplayId;
-  //       this.setState({
-  //         bossImageUrl :'https://render-us.worldofwarcraft.com/npcs/zoom/creature-display-' + creatureDisplayId + '.jpg'
-  //       });
-  //     }).catch((err) => {
-  //       console.log(err);
-  //   });
-  // }
-  // const following = _.map(_.take(this.props.profile.following, 3), (following) => (
-  //     <img key={following.id} className={styles.image} src={`${baseUrl}/30x30/${following.image}`} />
-  //   ));
 
-  // <ul>
-  // {_.map(spellID, (spell, index) =>
-  //   <TalentItems spell={spell} key={index} />
-  // )}
-  // </ul>
   render() {
     const { log, progress } = this.props;
     const bestPercentage = log.specs[0].best_historical_percent;
@@ -44,11 +20,9 @@ class Logs extends React.Component {
     const spellID = _.map(talents, 'id');
 
     return (
-      <tbody>
-        <td className={styles.tableCenter}>
-          <span className={styles.textCenter}>{bestPercentage}</span>
-        </td>
-      </tbody>
+      <td className={styles.tableCenter}>
+        <span className={styles.textCenter}>{bestPercentage}</span>
+      </td>
     )
   }
 
