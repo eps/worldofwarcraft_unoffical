b67af941310689fce00eaebf752bf76ba87c217d
Herman Chan <hermchan89@gmail.com>
2017-12-08 14:44:08 -0800

    fixed bugs for deployment in webpack and packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,7 @@
-// const webpack = require('webpack');
-// const path = require('path');
-//
-// module.exports = {
-//     entry: [
-//       './src/index.js'
-//     ],
-//     output: {
-//       path: path.resolve(__dirname, 'build'),
-//       publicPath: "/assets/",
-//       filename: "bundle.js"
-//     },
-//     module: {
-//         loaders: [
-//             {
-//               test: /\.(eot|woff|woff2|svg|ttf|png)([\?]?.*)$/,
-//               loader: "file-loader"
-//             },
-//             {
-//               test: /\.html$/,
-//               loader: 'file-loader?name=[name].[ext]',
-//             },
-//             {
-//               test: /\.jsx?$/,
-//               exclude: /node_modules/,
-//               loader: 'babel-loader',
-//               query: {
-//                 presets: ['es2015', 'react']
-//               }
-//             },
-//             {
-//               test: /\.scss$/,
-//               loaders: ['style-loader', 'css-loader?url=false', 'sass-loader']
-//             }
-//         ],
-//     },
-//     devServer: {
-//     contentBase: "./public",
-//     hot: true
-//   },
-//     plugins: [
-//       new webpack.NamedModulesPlugin(),
-//       new webpack.NoEmitOnErrorsPlugin(),
-//     ]
-// };
-
-
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const Uglify = require("uglifyjs-webpack-plugin");
 
 const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 const APP_DIR = path.resolve(__dirname, 'src/client/app');
@@ -58,20 +12,28 @@ const config = {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
+  watch: true,
   module : {
-   loaders : [
-     {
-       test : /\.jsx?/,
-       include : APP_DIR,
-       loader : 'babel-loader'
-     },
-     {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /.(scss|css)$/,
         loader: 'style-loader!css-loader?modules&camelCase=dashes&sourceMap&localIdentName=[local]___[hash:base64:5]!sass-loader?outputStyle=expanded&sourceMap'
       }
-   ]
- },
-  plugins: [
+    ]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new Uglify(),
     new ExtractTextPlugin('styles.css')
   ]
 }
